9bdccb367b4ce785ff48af012cf4c3ae74e097b9
Max Greenwald <maxgreenwald@me.com>
2019-08-22 16:53:23 -0400

    Figure out object naming

--- index.js
+++ index.js
@@ -2,18 +2,3 @@
 
 import * as Flow from './lib';
 export { Flow };
-
-const J = Flow.Object({
-  n: Flow.string,
-  y: Flow.number,
-});
-
-const q = Flow.Object(Flow.number, {
-  n: Flow.string,
-  y: Flow.number,
-});
-
-const f = q.validate(10);
-const n = f.y;
-
-const z = Flow.omap(Flow.number);
